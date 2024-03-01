4eca156b50dc19acdcabb2e18aaceb7315ad3fce
Ashutosh <ashutoshshinde1998@gmail.com>
2024-03-01 19:40:47 +0530

    Sidenav component added

---
+++ src/app/budget-planner/side-nav/side-nav.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-side-nav',
+  standalone: true,
+  imports: [MatIconModule,CommonModule],
+  templateUrl: './side-nav.component.html',
+  styleUrl: './side-nav.component.scss',
+})
+export class SideNavComponent {
+  isSlideOut = true;
+
+  constructor(private router: Router) {}
+
+  toggleSlideOut(): void {
+    this.isSlideOut = !this.isSlideOut;
+  }
+
+  onDash() {
+    this.isSlideOut = !this.isSlideOut;
+  }
+
+  onProfile() {
+    this.router.navigate(['/budget-planner/profile']);
+  }
+
+  onHistory() {
+    this.router.navigate(['/budget-planner/history']);
+  }
+
+  onLogOut() {
+    this.router.navigate(['/budget-planner/login']);
+  }
+}
